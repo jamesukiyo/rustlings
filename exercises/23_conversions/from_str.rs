ace2ca828ddac362e89fdc77436b18c988ca666a
James Plummer <jamesp2001@live.co.uk>
2025-06-05 20:43:16 +0200

    91

--- exercises/23_conversions/from_str.rs
+++ exercises/23_conversions/from_str.rs
@@ -41,7 +41,26 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
 
-    fn from_str(s: &str) -> Result<Self, Self::Err> {}
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let split: Vec<_> = s.split(",").collect();
+
+        if split.len() != 2 {
+            return Err(ParsePersonError::BadLen);
+        }
+
+        if split[0].is_empty() {
+            return Err(ParsePersonError::NoName);
+        }
+
+        match split[1].parse::<u8>() {
+            Ok(age) => Ok(Person {
+                name: split[0].to_string(),
+                age,
+            }),
+
+            Err(error) => Err(ParsePersonError::ParseInt(error)),
+        }
+    }
 }
 
 fn main() {
